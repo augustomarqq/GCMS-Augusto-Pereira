4688e0ab223d9464dc978e2ef261031b0f448f66
augustomarqq <augustomarqq@gmail.com>
2024-08-04 04:00:28 -0300

    primeiro commit

---
+++ Pratica02/WebApp/src/main/java/br/edu/ifpe/recife/gcms/WebApp/HomeController.java
@@ -0,0 +1,18 @@
+package br.edu.ifpe.recife.gcms.WebApp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public @ResponseBody String greeting() {
+		return "Olá, mundo!";
+	}
+	
+	@RequestMapping("/augusto")
+	public @ResponseBody String greetingFulano() {
+	return "Olá, Augusto!";
+	}
+}
